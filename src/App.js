de34ac46fc45ac988936519a222b8ac5b4e428c6
KlunkyKong <danielhjermitslev@protonmail.com>
2021-01-28 12:56:24 +0100

    add token context provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Router } from '@reach/router';
-import { Router } from "@reach/router";
 import Login from './pages/Login';
 import Callback from './pages/Callback';
 import TokenContext from './TokenContext';
@@ -7,10 +6,12 @@ import './App.css';
 
 function App() {
   return (
+    <TokenContext.Provider value={tokenState}>
     <Router>
       <Login default />
       <Callback path="/callback" />
     </Router>
+    </TokenContext.Provider>
   );
 }
 
