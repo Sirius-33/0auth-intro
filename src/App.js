e92e81fb5a1a86f451e05589367f23a66f1a0ff2
KlunkyKong <danielhjermitslev@protonmail.com>
2021-01-28 23:06:41 +0100

    --add last bits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import { Router } from '@reach/router';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
 import TokenContext from './TokenContext';
 import './App.css';
+import Featured from "./pages/Featured";
+
 
 function App() {
+  var tokenState = useState(null);
+
   return (
     <TokenContext.Provider value={tokenState}>
     <Router>
       <Login default />
       <Callback path="/callback" />
+      <Featured path="/featured" />
     </Router>
     </TokenContext.Provider>
   );
--- src/pages/Callback.js
+++ src/pages/Callback.js
@@ -1,12 +1,23 @@
 import axios from "axios";
+import { navigate } from "@reach/router";
+import { useContext, useEffect } from "react";
+import TokenContext from "../TokenContext";
 
-export default function Callback( props ) {
+export default function Callback(props) {
+  var [token, setToken] = useContext(TokenContext);
+
   var code = new URLSearchParams(props.location.search).get("code");
-  
-  axios.post("/.netlify/functions/token", JSON.stringify({
+
+  useEffect(function () {
+    axios.post("/.netlify/functions/token", JSON.stringify({
       code
-    
-  }))
-  .then(response => console.log(response));
-  return null;
+
+    }))
+      .then(response => {
+        setToken(response.data);
+        navigate("/featured");
+
+    })}, [setToken, code]);
+
+    return null;
 }
